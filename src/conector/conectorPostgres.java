d982bc28029a815e92a049a6957887965e4fe4c1
Stel0r <65517739+Stel0r@users.noreply.github.com>
2022-12-04 15:59:56 -0500

    first Commit

---
+++ src/conector/conectorPostgres.java
@@ -0,0 +1,11 @@
+package conector;
+
+import java.sql.*;
+
+public class conectorPostgres {
+	
+	public conectorPostgres() {
+		
+	}
+
+}
---
+++ src/logica/Main.java
@@ -0,0 +1,11 @@
+package logica;
+
+import conector.conectorPostgres;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		conectorPostgres conector = new conectorPostgres();
+	}
+
+}
