a2a9c688f40838fe5b6e16a947af0b90e457f036
Stel0r <65517739+Stel0r@users.noreply.github.com>
2022-12-05 06:54:54 -0500

    creada conexion
    agregada la estructura basica para una query
    actualmente se conecta a la base vista en clase de alrel

--- src/conector/conectorPostgres.java
+++ src/conector/conectorPostgres.java
@@ -4,10 +4,17 @@ import java.sql.*;
 
 public class conectorPostgres {
 	
-	private String URL;
+	private String URL = "jdbc:postgresql://localhost:5432/alrel";
+	private String user = "postgres";
+	private String password = "basedatos";
 	
 	public conectorPostgres() throws SQLException {
-		Connection conexion = DriverManager.getConnection(URL);
+		Connection conexion = DriverManager.getConnection(URL,user,password);
+		String query = "SELECT * FROM cli_compensar";
+		Statement queryStatement = conexion.createStatement();
+		ResultSet result = queryStatement.executeQuery(query);
+		result.next();
+		System.out.println(result.getString(3));
 	}
 
 }
